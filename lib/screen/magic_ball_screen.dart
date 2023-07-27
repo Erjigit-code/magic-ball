103b9f562a573f0e3a5bdb20c5909c05a2f3b731
Erjigit Abdykaarov <erjigit1003@gmail.com>
2023-07-27 17:46:10 +0600

    added animation

--- lib/screen/magic_ball_screen.dart
+++ lib/screen/magic_ball_screen.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:surf_practice_magic_ball/api/api_service.dart';
-import 'package:shake/shake.dart';
 import 'dart:math';
+
+import 'package:animated_text_lerp/animated_text_lerp.dart';
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
+
+import 'package:surf_practice_magic_ball/api/api_service.dart';
 
 import '../widgets/footer.dart';
 import '../widgets/responses.dart';
@@ -14,7 +18,9 @@ class MagicBallScreen extends StatefulWidget {
   State<MagicBallScreen> createState() => _MagicBallScreenState();
 }
 
-class _MagicBallScreenState extends State<MagicBallScreen> {
+class _MagicBallScreenState extends State<MagicBallScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
   List<String> serverResponses = MagicBallResponses.serverResponses;
   Map<String, String> replacements = getReplacements();
 
@@ -26,14 +32,19 @@ class _MagicBallScreenState extends State<MagicBallScreen> {
 
   @override
   void initState() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 50),
+      vsync: this,
+    );
+    _controller.repeat();
     super.initState();
 
     ShakeDetector detector = ShakeDetector.autoStart(
       onPhoneShake: () async {
         onTap();
       },
-      minimumShakeCount: 1,
-      shakeSlopTimeMS: 500,
+      minimumShakeCount: 2,
+      shakeSlopTimeMS: 1000,
       shakeCountResetTime: 3000,
       shakeThresholdGravity: 2.7,
     );
@@ -59,13 +70,11 @@ class _MagicBallScreenState extends State<MagicBallScreen> {
           GestureDetector(
             onTap: onTap,
             child: tapped
-                ? ShowAnswer(responseTextShow: showText)
-                : Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Image.asset('assets/images/ball-dark.png'),
-                    ],
-                  ),
+                ? AnswerClass(
+                    responseTextShow: showText,
+                    controller: _controller,
+                  )
+                : RotatingObject(controller: _controller),
           ),
           Stack(
             alignment: Alignment.center,
@@ -111,5 +120,92 @@ class _MagicBallScreenState extends State<MagicBallScreen> {
     final randomResponse = serverResponses[randomIndex];
 
     return replacements[randomResponse] ?? '';
+  }
+}
+
+class RotatingObject extends StatelessWidget {
+  const RotatingObject({
+    super.key,
+    required AnimationController controller,
+  }) : _controller = controller;
+
+  final AnimationController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.asset('assets/images/ball.png'),
+        RotationTransition(
+          turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+          child: Image.asset(
+            'assets/images/smallstar.png',
+          ),
+        ),
+        RotationTransition(
+          turns: Tween(begin: 1.0, end: 0.0).animate(_controller),
+          child: Image.asset('assets/images/star.png'),
+        ),
+      ],
+    );
+  }
+}
+
+class AnswerClass extends StatefulWidget {
+  final String responseTextShow;
+  const AnswerClass({
+    super.key,
+    required AnimationController controller,
+    required this.responseTextShow,
+  }) : _controller = controller;
+
+  final AnimationController _controller;
+
+  @override
+  State<AnswerClass> createState() => _AnswerClassState();
+}
+
+class _AnswerClassState extends State<AnswerClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.asset('assets/images/ball.png'),
+        RotationTransition(
+          turns: Tween(begin: 0.0, end: 1.0).animate(widget._controller),
+          child: Image.asset(
+            'assets/images/smallstar.png',
+          ),
+        ),
+        RotationTransition(
+          turns: Tween(begin: 1.0, end: 0.0).animate(widget._controller),
+          child: Image.asset(
+            'assets/images/star.png',
+            width: 200,
+            height: 200,
+          ),
+        ),
+        Image.asset('assets/images/black.png'),
+        SizedBox(
+          width: 243,
+          child: Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: AnimatedStringText(
+                widget.responseTextShow,
+                textAlign: TextAlign.center,
+                duration: Duration(milliseconds: 700),
+                curve: Curves.decelerate,
+                style: const TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 32,
+                    fontFamily: 'Sans',
+                    fontWeight: FontWeight.w400,
+                    color: Color.fromARGB(255, 252, 250, 252)),
+              )),
+        ),
+      ],
+    );
   }
 }
--- lib/widgets/show_answer.dart
+++ lib/widgets/show_answer.dart
@@ -1,33 +1,57 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:animated_text_lerp/animated_text_lerp.dart';
 import 'package:flutter/material.dart';
+import 'package:surf_practice_magic_ball/screen/magic_ball_screen.dart';
 
-class ShowAnswer extends StatelessWidget {
+class ShowAnswer extends StatefulWidget {
   final String responseTextShow;
 
   const ShowAnswer({super.key, required this.responseTextShow});
 
   @override
+  State<ShowAnswer> createState() => _ShowAnswerState();
+}
+
+class _ShowAnswerState extends State<ShowAnswer> {
+  @override
   Widget build(BuildContext context) {
     return Stack(
       alignment: Alignment.center,
       children: [
-        Image.asset('assets/images/ball-dark.png'),
-        Image.asset('assets/images/Vector2.png'),
+        // Image.asset('assets/images/ball-dark.png'),
+        // Image.asset(
+        //   'assets/images/Vector.png',
+        //   height: 200,
+        //   width: 200,
+        // ),
         SizedBox(
           width: 243,
           child: Padding(
-            padding: const EdgeInsets.only(top: 15.0),
-            child: Text(
-              responseTextShow, // Display the translated response from the API
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 32,
-                fontFamily: 'Sans',
-                fontWeight: FontWeight.w400,
-                color: Color(0xFFFFFFFF),
+              padding: const EdgeInsets.only(top: 15.0),
+              child: AnimatedStringText(
+                widget.responseTextShow,
+                textAlign: TextAlign.center,
+                duration: Duration(milliseconds: 700),
+                curve: Curves.decelerate,
+                style: const TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 32,
+                    fontFamily: 'Sans',
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff9923A2)),
+              )
+              // Text(
+              //   responseTextShow, // Display the translated response from the API
+              //   textAlign: TextAlign.center,
+              // style: const TextStyle(
+              //   decoration: TextDecoration.none,
+              //   fontSize: 32,
+              //   fontFamily: 'Sans',
+              //   fontWeight: FontWeight.w400,
+              //   color: Color(0xFFFFFFFF),
+              // ),
+              // ),
               ),
-            ),
-          ),
         ),
       ],
     );
